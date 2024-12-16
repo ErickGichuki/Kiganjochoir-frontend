ed297b329777744bfa910f17d9e8a02369888e5f
+ <+>
2024-12-16 08:58:24 +0300

    refactor:modify the songs page

--- app/recordedsongs/page.js
+++ app/recordedsongs/page.js
@@ -1,30 +1,54 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function Page() {
     const [songs, setSongs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
     useEffect(() =>{
-        axios.get('https://kiganjochoir.onrender.com/recordedsongs/')
-            .then(response => {
-                setSongs(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching songs:', error);
-            });
+      const fetchSongs = async () => {
+        try {
+          const response = await axios.get(
+            "https://kiganjochoir.onrender.com/recordedsongs/"
+          );
+          setSongs(response.data);
+        } catch (error) {
+          console.error("Error fetching songs:", error);
+          setError("Failed to load songs. Please try again later.");
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchSongs();
     }, []);
   return (
     <div className='bg-gradient-to-r py-10'>
       <h2 className='font-bold text-center text-xl text-blue-700 my-4'>SDA Kiganjo Church Choir Songs</h2>
-      {songs.map(song => (
-        <div key={song.id} className='bg--500 rounded-full pb-10 mb-4 mx-4'>
+      {loading ? (
+        <div className='flex justify-center items-center'>
+          <AiOutlineLoading3Quarters className="text-4xl text-blue-700 animate-spin" />
+        </div>
+      ) : error ? (
+        <p className='text-center text-red-500'>{error}</p>
+      ) : songs.length === 0 ? (
+        <p className='text-center text-gray-500'>No songs available at the moment!!</p>
+      ): (
+        <div className='space-y-4'>
+          {songs.map(song => (
+          <div key={song.id} className='bg--500 rounded-full pb-10 mb-4 mx-4'>
             <h3 className='text-black text-center'>{song.title}</h3>
             <audio controls controlsList='nodownload' className=''>
                 <source src={song.audio} type='audio/mpeg' />
                 Your browser seems not to support the audio element!!!!
             </audio>
-        </div>
+          </div>
       ))}
+        </div> 
+      )}
     </div>
   )
 }
