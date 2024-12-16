81edfeb226ef9e6218ff07552dd68dd552f31e1a
+ <+>
2024-12-16 11:11:40 +0300

    feat:add songs dashboard

--- app/recordedsongs/page.js
+++ app/recordedsongs/page.js
@@ -25,9 +25,13 @@ function Page() {
   
       fetchSongs();
     }, []);
+
   return (
-    <div className='bg-gradient-to-r py-10'>
-      <h2 className='font-bold text-center text-xl text-blue-700 my-4'>SDA Kiganjo Church Choir Songs</h2>
+    <div className='bg-gradient-to-r from-blue-200 via-yellow-300 to-green-300 py-10'>
+      <h2 className='font-bold text-center text-2xl text-blue-700 my-6'>
+        SDA Kiganjo Church Choir Songs
+      </h2>
+      
       {loading ? (
         <div className='flex justify-center items-center'>
           <AiOutlineLoading3Quarters className="text-4xl text-blue-700 animate-spin" />
@@ -35,22 +39,22 @@ function Page() {
       ) : error ? (
         <p className='text-center text-red-500'>{error}</p>
       ) : songs.length === 0 ? (
-        <p className='text-center text-gray-500'>No songs available at the moment!!</p>
-      ): (
-        <div className='space-y-4'>
-          {songs.map(song => (
-          <div key={song.id} className='bg--500 rounded-full pb-10 mb-4 mx-4'>
-            <h3 className='text-black text-center'>{song.title}</h3>
-            <audio controls controlsList='nodownload' className=''>
+        <p className='text-center text-gray-500'>No songs available at the moment!</p>
+      ) : (
+        <div className='space-y-6'>
+          {songs.map((song, index) => (
+            <div key={song.id} className='bg-white shadow-lg rounded-lg pb-6 mb-4 mx-6 p-2'>
+              <h3 className='text-xl text-blue-800 font-semibold text-center'>{index + 1}. {song.title}</h3>
+              <audio controls controlsList='nodownload' className='w-full mt-4'>
                 <source src={song.audio} type='audio/mpeg' />
-                Your browser seems not to support the audio element!!!!
-            </audio>
-          </div>
-      ))}
-        </div> 
+                Your browser does not support the audio element.
+              </audio>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   )
 }
 
-export default Page
+export default Page;
